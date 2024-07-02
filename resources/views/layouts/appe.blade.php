8ed19849ac92e8b5e495435d4340f5e4f521b0a5
mareme <mariamathiaw92@gmail.com>
2024-07-02 10:21:22 +0000

    Intégration du navbar et du footer

--- resources/views/layouts/appe.blade.php
+++ resources/views/layouts/appe.blade.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="{{ asset('css/evenement_association.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
     <title>Gestion d'évènement et de réservation</title>
 </head>
 <body>
@@ -18,27 +19,61 @@
                     <a style="font-weight:700;font-size:30px;" href="/">Nom evenement</a>
                 </div>
                 <ul class="nav-links">
-                    <li><a style=" font-weight:400;font-size:24px;" href="{{ url('/') }}">Accueil</a></li>
-                    <li><a style=" font-weight:400;font-size:24px;"  href="/categories">Évènements</a></li>
+                    <li class="{{ Route::is('/') ? 'active-link' : '' }}">
+                        <a href="{{ url('/') }}">Accueil</a>
+                    </li>
+                    <li class="{{ Route::is('evenement.event') ? 'active-link' : '' }}">
+                        <a href="/evenement/event">Évènements</a>
+                    </li>
                 </ul>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    @guest<a href="/login" class="btn  btn-sm" style=" font-weight:400;font-size:16px;  background-color:#ffffff;
-                    color:#333;">Espace perso</a>@endguest
-                    @auth<a href="#" onclick="document.getElementById('logout-form').submit()" class="btn  btn-sm" style=" font-weight:400;font-size:16px; background-color:#ffffff;
-                    color:#333;">
-                    <form action="/logout" action="post" id="logout-form">@csrf</form>
-                     Se Déconnecter
-                     </a>@endauth
-                    <!--<form class="form-inline my-2 my-lg-0 ml-auto" action="{*{ route('livres.search') }}" method="GET">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Rechercher un livre" aria-label="Rechercher" name="query">
-                    </form>-->
+                <ul class=contact>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
             </div>
         </nav>    
-        </header>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <div class="avant_footer">
+            <div class="joindre">
+                <h2>Nom du plateform</h2>
+                <h4>Vous pouvez nous suivre</h4>
+                <div class="icone">
+                    <img src="{{ asset('images/facebook.png') }}" alt="" width="40" height="40">
+                    <img src="{{ asset('images/twitter.png') }}" alt=""  width="45" height="45">
+                    <img src="{{ asset('images/instagram.png') }}" alt=""  width="40" height="40">
+                    <img src="{{ asset('images/likedin.png') }}" alt=""  width="45" height="45">
+                </div>
+            </div>
+            <div class="newsletter">
+                <h4>Abonner vous à notre newsletter pour ne<br> rien rater de nos nouvelles</h4>
+                <form action="" method="POST">
+                    @csrf
+                    <label for="email"></label>
+                    <div class="input-group">
+                        <input type="email" id="email" name="email" required placeholder="Votre adresse email">
+                        <button type="submit" class="button_newsletter">S'abonner</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="footer_final">
+            <div class="line"></div>
+            <div class="copy">
+                <p>Copyrigth</p>
+                <img src="{{ asset('images/copyright.png') }}" alt="">
+                <p>2024</p>
+            </div>
+            <div class="line"></div>
+        </div>
+    </footer>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
 </body>
 </html>
